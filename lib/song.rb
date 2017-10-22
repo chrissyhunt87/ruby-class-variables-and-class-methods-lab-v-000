baa3541871d27130cbbebefb49feb4cd204306e0
Chrissy Hunt <chrissy.hunt@gmail.com>
2017-10-22 18:25:02 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,12 @@
 class Song
   attr_reader :name, :artist, :genre
+
+  @@count = 0
+
+  @@artists = []
+
+  @@genres = []
+  
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
